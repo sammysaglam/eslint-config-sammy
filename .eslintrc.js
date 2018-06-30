f4ba545a5afb231242d729d179fc3fcea08a8400
Sammy Saglam <sami@saglam.tk>
2018-06-30 16:32:21 +0100

    turned off 'no-process-env' rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -143,7 +143,7 @@ module.exports = {
 		'no-param-reassign': 'error',
 		'no-path-concat': 'error',
 		'no-plusplus': 'off',
-		'no-process-env': 'error',
+		'no-process-env': 'off',
 		'no-process-exit': 'error',
 		'no-proto': 'error',
 		'no-prototype-builtins': 'error',
