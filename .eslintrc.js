5d89421a04bb4687639d5b59c2095cc4e6ce2ca8
Sammy Saglam <sami@saglam.tk>
2018-07-01 13:32:29 +0100

    added support for flow

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,18 +20,68 @@ module.exports = {
 		'sourceType': 'module'
 	},
 	'plugins': [
+		'import',
 		'react',
 		'prettier',
-		'jest'
+		'jest',
+		'flowtype'
 	],
 	'settings': {
-		'propWrapperFunctions': ['forbidExtraProps']
-	},
+		'propWrapperFunctions': ['forbidExtraProps'],
+		'flowtype': {
+			'onlyFilesWithFlowAnnotation': true,
+		}
 	"globals": {
         "process": false,
         "__dirname": false
     },
 	'rules': {
+		// ------------------------------------------------
+		// flow
+		// ------------------------------------------------
+		'flowtype/boolean-style': [2, 'boolean'],
+		'flowtype/define-flow-type': 1,
+		'flowtype/delimiter-dangle': [2, 'always-multiline'],
+		'flowtype/generic-spacing': [2, 'never'],
+		'flowtype/no-primitive-constructor-types': 2,
+		'flowtype/no-types-missing-file-annotation': 2,
+		'flowtype/no-weak-types': 2,
+		'flowtype/object-type-delimiter': [2, 'comma'],
+		'flowtype/require-parameter-type': 'off',
+		'flowtype/require-return-type': 'off',
+		'flowtype/require-valid-file-annotation': [
+			2,
+			'always',
+			{
+				'annotationStyle': 'line',
+			},
+		],
+		'flowtype/semi': [2, 'always'],
+		'flowtype/space-after-type-colon': [2, 'always'],
+		'flowtype/space-before-generic-bracket': [2, 'never'],
+		'flowtype/space-before-type-colon': [2, 'never'],
+		'flowtype/type-id-match': [2, '^([A-Z][a-z0-9]+)+Type$'],
+		'flowtype/union-intersection-spacing': [2, 'always'],
+		'flowtype/use-flow-type': 1,
+		'flowtype/valid-syntax': 1,
+		'import/no-duplicates': 'error',
+		'flowtype/newline-after-flow-annotation': [2, 'never'],
+		'flowtype/no-dupe-keys': 'error',
+		'flowtype/no-existential-type': 'error',
+		'flowtype/no-mutable-array': 'error',
+		'flowtype/no-unused-expressions': 'error',
+		'flowtype/sort-keys': [
+			2,
+			'asc',
+			{
+				'caseSensitive': true,
+				'natural': true,
+			},
+		],
+		// ------------------------------------------------
+		// ------------------------------------------------
+		// ------------------------------------------------
+
 		'prettier/prettier': 'error',
 		'accessor-pairs': 'error',
 		'array-callback-return': 'error',
@@ -107,7 +157,7 @@ module.exports = {
 		'no-catch-shadow': 'error',
 		'no-continue': 'error',
 		'no-div-regex': 'error',
-		'no-duplicate-imports': 'error',
+		'no-duplicate-imports': 'off',
 		'no-else-return': 'error',
 		'no-empty-function': 'error',
 		'no-eq-null': 'error',
