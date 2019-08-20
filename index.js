0d218e02b7b28049422c59a5ec7e2b40e514ba9d
Sammy Saglam <sammy@saglam.cc>
2019-08-20 08:55:27 +0200

    turned off rule "no-underscore-dangle"

--- index.js
+++ index.js
@@ -255,7 +255,7 @@ module.exports = {
 		'no-throw-literal': 'error',
 		'no-undef-init': 'error',
 		'no-undefined': 'off',
-		'no-underscore-dangle': 'error',
+		'no-underscore-dangle': 'off',
 		'no-unmodified-loop-condition': 'error',
 		'no-unneeded-ternary': 'error',
 		'no-unused-expressions': 'error',
