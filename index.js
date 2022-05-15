65049228bfcbf7cc6e100664bf3163ee49146f16
Sammy Saglam <sammy@saglam.co>
2022-05-15 17:48:36 +0000

    feat: enable rule 'no-explicit-any'

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ module.exports = {
     "@typescript-eslint/camelcase": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-member-accessibility": "off",
-    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-explicit-any": "error",
     "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/no-var-requires": "off",
     "@typescript-eslint/no-namespace": "off",
